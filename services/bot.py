6e8c45f5eaa126585c6a1ee85462be544c643952
BITLIFASON <vladov.ru@gmail.com>
2024-01-05 12:30:03 +0300

    add beginner mode

--- services/bot.py
+++ services/bot.py
@@ -14,44 +14,84 @@ from telegram.ext import (
 from langchain.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-
 
 
 with open('../credentials.json', 'r') as f:
     credentials = json.load(f)
 
+
 OPENAI_API_KEY = credentials["OPENAI_API_KEY"]
 TELEGRAM_TOKEN = credentials["TELEGRAM_TOKEN"]
 
-embeddings=OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
 vectorstore = Chroma(collection_name='engineering_store',
                      embedding_function=embeddings,
                      persist_directory="../db")
 collection = vectorstore.get()
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
+
+setup_and_retrieval = RunnableParallel({"context": retriever, "question": RunnablePassthrough()})
+
+
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=OPENAI_API_KEY)
 
-setup_and_retrieval = RunnableParallel(
-    {"context": retriever, "question": RunnablePassthrough()}
-)
-template = "Контекст: {context}\n\n"\
-           "Используя контекст, ответь на вопрос: {question}."\
-           "Если в данном контексте нет нужной информации, то отвечай:\n"\
-           "Извините, не нашёл информации по этому вопросу"
 
-
+# шаблон для обычного ответа
+template = """Контекст: {context}\n\n\
+Используя контекст, ответь на вопрос: {question}. \
+Если в данном контексте нет нужной информации, то отвечай:\n"\
+Извините, не нашёл информации по этому вопросу"""
 prompt = ChatPromptTemplate.from_template(template)
+
+
+# системный шаблон для учёта контекста истории чата
+contextualize_system_template = """Учитывая историю чата и последний вопрос пользователя, \
+который может ссылаться на контекст в истории чата, сформулируй отдельный вопрос, \
+который можно понять без истории чата. НЕ отвечай на этот вопрос, \
+просто переформулируй его, если необходимо, и в противном случае верни как есть."""
+contextualize_system_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_system_template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+contextualize_chain = contextualize_system_prompt | llm | StrOutputParser()
+
+# системный шаблон для ответа в режиме для начинающих
+system_template = """Ты ассистент для ответов на вопросы. \
+Используй следующие фрагменты извлеченного контекста, чтобы ответить на вопрос {context}\n\
+Если в данном контексте нет нужной информации, то отвечай:\n\
+Детализируйте свой вопрос"""
+system_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}"),
+    ]
+)
+def contextualized_question(input: dict):
+    if input.get("chat_history"):
+        return contextualize_chain
+    else:
+        return input["question"]
+contextualized_retrieval = RunnablePassthrough.assign(context=contextualized_question | retriever)
+
+
 output_parser = StrOutputParser()
+
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-CHOOSING, DONE, ANSWER, BEGINNER, OLD, SMART, ERROR = range(7)
+CHOOSING, DONE, ANSWER, BEGINNER, BEGINNER_CHOICE, OLD, SMART, ERROR = range(8)
 
 reply_keyboard = [
     ["Получить ответ на вопрос"],
@@ -62,6 +102,8 @@ reply_keyboard = [
     ["Закончить"],
 ]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+markup_begin = ReplyKeyboardMarkup([['Уточнить детали вопроса', 'Вернуться в меню']], one_time_keyboard=True)
+
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -91,6 +133,7 @@ async def choosing(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return ANSWER
 
     if text == "Режим для начинающих":
+        await update.message.reply_text("Введите вопрос:")
         return BEGINNER
 
     if text == "Ответ по старым версиям":
@@ -112,6 +155,7 @@ async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global retriever
     global setup_and_retrieval
     global prompt
+    global llm
     global output_parser
 
     user = update.message.from_user.username
@@ -144,15 +188,54 @@ async def answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def beginner(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """"Beginner mode"""
 
+    global contextualized_retrieval
+    global llm
+
     user = update.message.from_user.username
+    user_data = context.user_data
     logger.info("User %s call beginner.", user)
 
-    await update.message.reply_text(
-        "Функция не реализована. Выберите пункт меню.",
-        reply_markup=markup
-    )
+    if not user_data.get('chat_history', False):
+        user_data['chat_history'] = []
 
-    return CHOOSING
+    text = update.message.text
+
+    context = contextualized_retrieval.invoke({"question": text,
+                                               "chat_history": user_data['chat_history']})
+    prompt_with_context = system_prompt.invoke(context)
+    llm_answer = llm.invoke(prompt_with_context)
+    llm_output = output_parser.invoke(llm_answer)
+    user_data['chat_history'].extend([HumanMessage(content=text), llm_answer])
+
+    await update.message.reply_text(llm_output)
+
+    relevant_documents = []
+    for doc in context['context']:
+        doc_name = doc.metadata["source"].split("\\")[-1][:-4]
+        if doc_name not in relevant_documents:
+            relevant_documents.append(doc_name)
+    await update.message.reply_text("Список возможно подходящих документов:\n"+"\n".join(relevant_documents))
+
+    await update.message.reply_text("Сделайте выбор.", reply_markup=markup_begin)
+
+    return BEGINNER_CHOICE
+
+
+async def beginner_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """"Choice beginner"""
+
+    user = update.message.from_user.username
+    user_data = context.user_data
+    logger.info("User %s call beginner choice.", user)
+
+    text = update.message.text
+
+    if text == "Уточнить детали вопроса":
+        await update.message.reply_text("Введите детали:")
+        return BEGINNER
+    else:
+        user_data['chat_history'] = []
+        return CHOOSING
 
 
 async def old(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -222,6 +305,7 @@ def main() -> None:
             CHOOSING: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), choosing)],
             ANSWER: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), answer)],
             BEGINNER: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), beginner)],
+            BEGINNER_CHOICE: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), beginner_choice)],
             OLD: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), old)],
             SMART: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), smart)],
             ERROR: [MessageHandler(filters.TEXT & ~filters.Regex('^Закончить$'), error)]
